6bacac5b1b188da5a977c7c55ef84915c69bf34f
rafi-gdk <rafi.gdk@gmail.com>
2022-06-20 11:15:17 +0530

    Added basic files

---
+++ src/test/java/com/sonu/security/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonu.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
